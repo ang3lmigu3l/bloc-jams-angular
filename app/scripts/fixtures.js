be981c99350c92fb07921a440fa0d904741dc25e
Miguel Angel Quintana <quintana.mige@gmail.com>
2016-12-11 01:25:58 -0800

    Assignment completed

--- app/scripts/fixtures.js
+++ app/scripts/fixtures.js
@@ -2,7 +2,7 @@
      title: 'The Colors',
      artist: 'Pablo Picasso',
      label: 'Cubism',
-     year: '1881',
+     year: '1882',
      albumArtUrl: 'assets/images/album_covers/01.png',
      songs: [
          { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
